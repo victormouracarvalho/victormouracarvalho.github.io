f476eb90beeb2434002fee85ed9607d2d6d536a5
victormouracarvalho <48720986+victormouracarvalho@users.noreply.github.com>
2019-05-01 17:58:13 -0300

    Update respJS_001.js

--- respJS_001.js
+++ respJS_001.js
@@ -15,7 +15,7 @@ function Q2() {
     var x = Number.parseFloat(document.getElementById('b').value);
     var y = Number.parseFloat(document.getElementById('c').value);
     // Segue aqui seu código
-    console.log ('b,c ' + x + ' / ' + y);
+    console.log ('b e c =' + ' ' + x + ' / ' + y);
     console.log ('Hipotenusa = ' +  Math.sqrt(Math.pow(x,2) + Math.pow(y,2)));
 }
 
@@ -28,7 +28,9 @@ function Q3() {
     var aux = document.getElementById('profissoes');
     var profissao = aux.options[aux.selectedIndex].text;
     // Segue aqui seu código
-    if (profissao .endsWith('a')) {
+    if (profissao .endsWith('Musicista')) {
+        console.log(profissao + ' ' + 'substantivo masculino')
+    } else if (profissao .endsWith('a')) {
         console.log (profissao + ' ' + 'substantivo feminino');
     } else if (profissao .endsWith('o')) {
         console.log (profissao + ' ' + 'substantivo masculino');
